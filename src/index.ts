7f63fafde9a854c06253164953b5a45ef7a8a813
William Kang <william@wkang.ca>
2025-01-18 06:52:33 -0500

    start auth

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,32 @@
 import { S3Client } from 'bun';
 import express from 'express';
+import { auth } from 'express-openid-connect';
 import multer from 'multer';
 import bodyParser from 'body-parser';
 import { initRoutes } from './routes.ts';
 
-if (!process.env.S3_ACCESS_KEY_ID || !process.env.S3_SECRET_ACCESS_KEY) {
-    throw new Error('S3_ACCESS_KEY_ID and S3_SECRET_ACCESS_KEY must be set in .env');
-}
+if (!process.env.S3_ACCESS_KEY_ID || !process.env.S3_SECRET_ACCESS_KEY || !process.env.S3_ENDPOINT || !process.env.S3_BUCKET || !process.env.AUTH0_DOMAIN || !process.env.AUTH0_SECRET || !process.env.AUTH0_CLIENT_ID)
+    throw new Error('Missing environment variables');
 
 export const app: express.Express = express();
 export const upload: multer.Multer = multer({ storage: multer.memoryStorage() });
 export const r2 = new S3Client({
     accessKeyId: process.env.S3_ACCESS_KEY_ID,
     secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-    bucket: 'snapquest',
-    endpoint: 'https://a7c649bdd7eeb9c55573d2f393768828.r2.cloudflarestorage.com'
+    bucket: process.env.S3_BUCKET,
+    endpoint: process.env.S3_ENDPOINT
 });
 
 if (app.get('env') === 'production') app.set('trust proxy', 1);
 
+app.use(auth({
+    authRequired: false,
+    auth0Logout: true,
+    baseURL: 'http://localhost:3000',
+    clientID: process.env.AUTH0_CLIENT_ID,
+    issuerBaseURL: process.env.AUTH0_DOMAIN,
+    secret: process.env.AUTH0_SECRET
+}));
 app.use(bodyParser.json());
 app.use(express.static('src/public'));
 
