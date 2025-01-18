4f1ac5e79c35bf18b5bea8457980eb29889d997a
William Kang <william@wkang.ca>
2025-01-18 06:05:30 -0500

    prevent uuid collision

--- src/api/get/upload.ts
+++ src/api/get/upload.ts
@@ -3,6 +3,11 @@ import { randomUUID } from 'node:crypto';
 
 export default app.get('/api/upload/', async (_req, res) => {
     try {
+        let uuid = randomUUID();
+        const check = r2.file(uuid);
+        while (await check.exists())
+            uuid = randomUUID();
+
         const uploadUrl = r2.presign(randomUUID(), {
             method: 'PUT',
             expiresIn: 3600,
