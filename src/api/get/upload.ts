4b1e2daf96289b8df55ec68d395df2853c8fb621
William Kang <william@wkang.ca>
2025-01-18 06:02:51 -0500

    client sided uploading

---
+++ src/api/get/upload.ts
@@ -0,0 +1,18 @@
+import { app, r2 } from '../../index.ts';
+import { randomUUID } from 'node:crypto';
+
+export default app.get('/api/upload/', async (_req, res) => {
+    try {
+        const uploadUrl = r2.presign(randomUUID(), {
+            method: 'PUT',
+            expiresIn: 3600,
+            acl: 'public-read'
+        });
+        res.status(200).send(uploadUrl);
+        return;
+    } catch (error) {
+        console.log(error.stack ?? error);
+        res.status(500).send(error);
+        return;
+    }
+});
